da7091f3718e2fbe781837f934d4b5679d9e10b6
flovearth <flovearth@protonmail.com>
2021-11-01 11:09:35 +0000

    if state removed.

--- README.md
+++
@@ -1,2 +0,0 @@
-# 19_github-actions
-This repo is a workflow that runs a complete infrastructure on AWS.
--- outputs.tf
+++
@@ -1,6 +0,0 @@
-output "aws_instance_public_ip" {
-
-# output "alb-dns-name" {
-#  value = aws_alb.fp-alb.dns_name
-  value = aws_instance.jenkins.public_ip
-}
